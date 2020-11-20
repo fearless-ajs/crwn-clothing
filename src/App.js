9c339e67c5eeb89309ea1cf664df8ca914cb679f
Adurotimi Joshua <adurotimijoshua@gmail.com>
2020-11-20 21:57:22 +0100

    Update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 import Homepage from "./pages/homepage/homepage.component";
+
+const Hatspage = () => (
+    <div>
+        <h1>Hats page</h1>
+    </div>
+);
+
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route exact path='/hats' component={Hatspage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import {withRouter}  from  'react-router-dom';
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size}) => (
-    <div className={`${size} menu-item`}>
+
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match}) => (
+    <div className={`${size} menu-item`}
+         onClick={() => history.push(`${match.url}${linkUrl}`)}>
 
         {/*Div holding the background image*/}
         <div className='background-image'
@@ -18,4 +21,4 @@ const MenuItem = ({ title, imageUrl, size}) => (
     </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem); //To give access to history
