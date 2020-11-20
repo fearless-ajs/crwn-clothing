2640bc6afbf59f27d71709c2cb7737fc5c5ec500
Adurotimi Joshua <adurotimijoshua@gmail.com>
2020-11-20 16:34:42 +0100

    broken our homepage to smaller components, finised styling our menu-item

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './homepage.styles.scss';
+import Directory from '../../components/directory/directory.component';
+
+const Homepage = () => (
+    <div>
+        <Directory />
+    </div>
+);
+
+export default Homepage; //Export Homepage as our default homepage
